13d1101cc0b2bb56f4466d528608a30e1be85535
Wenbin Zhang <zhangwenbin0707@gmail.com>
2025-04-16 19:31:50 +0800

    增加了侧边栏

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,38 @@
 <template>
   <el-container style="height: 100vh">
-    <el-aside width="200px">
+    <!-- <el-aside width="200px">
       <el-menu default-active="1" class="el-menu-vertical-demo">
         <el-menu-item index="1">仪表盘</el-menu-item>
       </el-menu>
-    </el-aside>
+    </el-aside> -->
+    <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+      <el-menu-item index="1">
+        <el-icon><icon-menu /></el-icon>
+          <span>大屏展示</span>
+      </el-menu-item>
+      <el-sub-menu index="2">
+          <template #title>
+            <el-icon><location /></el-icon>
+            <span>系统设置</span>
+          </template>
+          <el-menu-item-group title="Group One">
+            <el-menu-item index="1-1">item one</el-menu-item>
+            <el-menu-item index="1-2">item two</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="Group Two">
+            <el-menu-item index="1-3">item three</el-menu-item>
+          </el-menu-item-group>
+          <el-sub-menu index="1-4">
+            <template #title>item four</template>
+            <el-menu-item index="1-4-1">item one</el-menu-item>
+          </el-sub-menu>
+        </el-sub-menu>
+    </el-menu>
 
     <el-container>
       <el-header>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
+import Dashboard from '@/views/Dashboard.vue'
 
 const routes = [{ path: '/', name: 'Dashboard', component: Dashboard }]
 
